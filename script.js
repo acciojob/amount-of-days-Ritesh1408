ef3e97e0ce51343e5f6da9e944d81fc665399416
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-01-30 22:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Enter a number: ", (num) => {
+  num = Number(num); 
+	function daysOfAYear(num) {
+		if(num % 4 === 0 && num % 100 !== 0 || num % 400 === 0){
+			return 366;
+		}else{
+			return 365
+		}
+	}
+  // console.log("You entered:", num);
+  rl.close();
+});
